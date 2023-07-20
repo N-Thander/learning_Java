2c294db0f9e2b83273a870052bdcb2c5f92792d6
N-Thander <108251321+N-Thander@users.noreply.github.com>
2023-07-20 18:33:01 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,38 @@
+class Variables
+{
+    public static void main(String a[])
+    {
+        // Note: Primitive variables are also known as literals
+        /*
+        int a = 10;
+        byte b = 127;
+        short c = 558;
+        long d = 5854l;
+        
+        float e = 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+        */
+
+       byte b = 127;
+       System.out.println(b);
+
+       // Type casting 
+       int z = 257;
+       byte k = (byte) z;
+       System.out.println(k);
+
+       // Type Emotion
+
+       byte m = 10;
+       byte n = 20;
+
+       int result = m*n;
+
+       System.out.println(result);
+
+    }
+}
