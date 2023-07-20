c007a05ac59c7792c5af39d19183c962238d041c
N-Thander <108251321+N-Thander@users.noreply.github.com>
2023-07-20 18:33:57 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,50 @@
+class Operators
+{
+    public static void main(String args[])
+    {
+        /*
+
+        int num1 = 7;
+        int num2 = 3;
+
+        // Arithmetic operations
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+        System.out.println(num1+=10);
+        System.out.println(num1++);
+
+        // num++ -> post increment
+        // ++num -> pre increment
+
+        */
+
+       /*
+       // Relational Operators
+
+       int m = 5;
+       int n = 10;
+
+       boolean result = m < n;
+       System.out.println(result);
+
+        */
+
+       /*
+       // Logical Operators -> note always use short circuit format i.e. && instead of &
+
+       int p = 10;
+       int q = 20;
+
+       int x = 50; 
+       int y = 60;
+
+       boolean output = p > q && x < q;
+       System.out.println(!output);
+
+        */
+
+    }
+}
